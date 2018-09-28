acc678f35d7527c5729437b0c86feb3cba72d471
Jefferson Heard <jheard@teamworks.com>
2018-09-28 16:02:02 -0400

    update setup

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jheard-tw/region_cache',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
